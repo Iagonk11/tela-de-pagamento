b78bbdbe022189655f2c871ea81c4b81df20775a
Iagonk11 <iago_koch@estudante.sesisenai.org.br>
2023-03-29 17:20:42 -0300

    asbfhi

---
+++ script.js
@@ -0,0 +1,21 @@
+'use strict';
+
+const payAmountBtn = document .querySelector('#payAmount');
+const decrementBtn = document .querySelector('#decrement');
+const quantityElem = document .querySelector('#quantity');
+const incrementBtn = document .querySelector('#increment');
+const priceElem = document .querySelector('#price');
+const SubtotalElem = document .querySelector('#Subtotal');
+const taxaElem = document .querySelector('#taxa');
+const totalElem = document .querySelector('#total');
+
+
+
+
+for (let i = 0; i < incrementBtn. lenght; i++) {
+
+incrementBtn[i].addEventListener('click', function (){
+    
+} )
+
+}
