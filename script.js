d45ad4e600d781cfe761b801b79dec03938ed93f
Iagonk11 <iago_koch@estudante.sesisenai.org.br>
2023-03-30 11:16:24 -0300

    aaaaaaaaaaaa

--- script.js
+++ script.js
@@ -16,6 +16,50 @@ for (let i = 0; i < incrementBtn. lenght; i++) {
 
 incrementBtn[i].addEventListener('click', function (){
     
-} )
+let increment = Number(this.previousElementSibling.textContent);
+
+increment++;
+
+this.previousElementSibling.textContent = increment;
+
+totalCalc();
+
+} );
+
+decrementBtn[i].addEventListener('click', function (){
+
+let decrement = Number(this.nextElementSibling.textContent);
+
+decrement <= 1 ? 1 : decrement--;
+
+this.nextElementSibling.textContent = decrement;
+
+totalCalc();
+
+});
+
+}
+
+const totalCalc = function() {
+    const tax = 0.1;
+    let subtotal = 0;
+    let totalTax = 0;
+    let total = 0;
+
+    for (let i = 0; i < quantityElem.lenght; i++){
+
+        subtotal += Number(quantityElem[i].textContent) * Number(priceElem[i].textContent);
+    }
+
+    SubtotalElem.textContent = subtotal.toFixed(2);
+
+    totalTax = subtotal * tax;
+
+   taxaElem.textContent = totalTax.toFixed(2);
+   
+   total = subtotal + totalTax;
+
+   totalElem.textContent = total.toFixed(2);
+   payAmountBtn.textContent = total.toFixed(2);
 
 }
